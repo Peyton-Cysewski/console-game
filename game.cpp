0ea93725319d47793f03b042b613e6258bcec0d6
Peyton Cysewski <github.peyton.cysewski@gmail.com>
2024-07-30 16:31:01 -0700

    Tinkering with console creation

--- game.cpp
+++ game.cpp
@@ -1,14 +1,71 @@
 #include "game.h"
 
-int main(void)
+#include <Windows.h>
+
+int Game::create_console(void)
 {
-	for (int i = 0; i < 100; i++)
-		fill();
+	LPCSTR name = "C:\dev\console-game\x64\Debug\console-game.exe";
+	char desktop[20] = "Desktop Placeholder";
+	char title[18] = "Title Placeholder";
+
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;;
+
+	si.lpReserved = NULL;
+	si.lpDesktop = desktop;
+	si.lpTitle = title;
+	si.dwX = 0; //STARTF_USEPOSTION in dwFlags
+	si.dwY = 0;
+	si.dwXSize = 100; //STARTF_USESIZE in dwFlags
+	si.dwYSize = 100;
+	si.dwXCountChars = 10; //STARTF_USECOUNTCHARS in dwFlags
+	si.dwYCountChars = 10;
+	si.dwFillAttribute = 0; //STARTF_USEFILLATTRIBUTE
+	si.dwFlags = 0;
+	si.wShowWindow = 0;
+	si.cbReserved2 = 0;
+	si.lpReserved2 = NULL;
+	si.hStdInput = 0;
+	si.hStdOutput = 0;
+	si.hStdError = 0;
+	si.cb = sizeof(si);
+
+	pi.hProcess = GetCurrentProcess();
+	pi.hThread = GetCurrentThread();
+	pi.dwProcessId = GetCurrentProcessId();
+	pi.dwThreadId = GetCurrentThreadId();
+
+
+	CreateProcessA(
+		name,
+		NULL,
+		NULL,
+		NULL,
+		FALSE,
+		CREATE_NEW_CONSOLE,
+		NULL,
+		NULL,
+		&si,
+		&pi
+	);
+
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
 	return 0;
 }
 
-int fill(void)
+int Game::run(void)
 {
-	std::cout << "AAAAAAAAAA";
+	Game::create_console();
+	return 0;
+}
+
+
+int main(void)
+{
+	Game texterminator = Game();
+	texterminator.run();
 	return 0;
 }
--- game.h
+++ game.h
@@ -2,8 +2,40 @@
 
 #include <iostream>
 
-// Entry Point
-int main(void);
+#include <Windows.h>
 
+// Goals
+// - Create a "simple" 2D Console game
+// - Exercise best practices and adhere to the Google C++ Style Guide to the best of my ability
+// - ***Don't let perfect me the enemy of good***
+
+// TODO
+// 
 // Create a Console
-int fill(void);
+// - Make it sizeable
+// - Interact with the console buffer
+// - Capture User K&M input
+// 
+// Gameplay Loop
+// - "Graphical" Main Menu
+// - Enter a game
+// - Play through a number a predetermined levels or until user "dies"
+// - Reutrn to main menu
+//
+// Extra Goals
+// - Create a savestate so that a game can be paused and fully exited and so that scores can be recorded
+// - Train an AI model to control the enemies
+
+// Notes
+// - Consoles are for applications that do not have their own GUIs
+// - May need to have a "character mode application" that runs the game...but the console is a "separate" (attached) application that provides user input and rendering output
+// ^this probably can just be handled in the same executable...it just has to reference itself???
+
+class Game
+{
+public:
+	int run(void);
+
+private:
+	int create_console(void);
+};
